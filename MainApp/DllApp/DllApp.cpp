7de6b619761262e479de0b2e5a029eeedaacedd7
seventh_attempt <asd99304@gmail.com>
2019-11-23 19:10:30 +0300

    connected dll

---
+++ MainApp/DllApp/DllApp.cpp
@@ -0,0 +1,10 @@
+// DllApp.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "DllApp.h"
+
+
+int SimpleSum(int a, int b) {
+	return a + b;
+}
---
+++ MainApp/DllApp/DllApp.h
@@ -0,0 +1,4 @@
+#pragma once
+#define EXPORT __declspec(dllexport)
+
+EXPORT int SimpleSum(int a, int b);
